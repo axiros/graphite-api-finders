ca6a8f257c54092ff3f56db2479d4f741eaf8537
stephan-hof <sh@axiros.com>
2016-12-19 14:38:05 +0100

    Initial commit with repository layout.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="ax.graphite_api_finders",
+    version="0.1",
+    url="https://github.com/axiros/graphite-api-finders",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['ax'],
+    install_requires=['setuptools']
+)
