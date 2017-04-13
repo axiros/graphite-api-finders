21448683f6a21dab90c9aa6955d4e8a47ffaefdb
stephan-hof <sh@axiros.com>
2017-04-13 15:50:44 +0200

    Tag 0.2 is there. Master is now 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="ax.graphite_api_finders",
-    version="0.2",
+    version="0.3",
     url="https://github.com/axiros/graphite-api-finders",
     packages=find_packages('src'),
     package_dir={'': 'src'},
